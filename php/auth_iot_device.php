f91db3eab9ce2793560fb71a9b00afbac34c7e2d
armoz14 <38024713+armorasha@users.noreply.github.com>
2020-01-20 22:40:50 +1030

    auth post added

--- php/auth_iot_device.php
+++ php/auth_iot_device.php
@@ -5,7 +5,7 @@ $config = parse_ini_file('/home/foodonya/r_admin_use/db2.ini'); //Online use
 
 $server_key = $config['POST_KEY'];
 
-    if ($server_key === $client_key) //when authentication fails
+    if ($server_key === $client_key) //when authentication pass
     {
         return true;
     }
--- php/post_data_receiver.php
+++ php/post_data_receiver.php
@@ -10,7 +10,6 @@ $client_key = $_POST['key'];
 ob_start();    // start output buffering
 include("auth_iot_device.php");
 $authentication = ob_get_contents();    // get contents from the buffer
-echo $authentication;
 ob_end_clean();   
 
     if ($authentication){
@@ -30,6 +29,7 @@ ob_end_clean();
     }
     else {
         echo "Authentication failed";
+        echo "auth" . $authentication;
     }
 
     
