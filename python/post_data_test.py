963427ca9bdda20283443e147e7ca611759af1dc
armoz14 <38024713+armorasha@users.noreply.github.com>
2020-01-20 21:36:46 +1030

    testing get/post

--- python/post_data_test.py
+++ python/post_data_test.py
@@ -3,7 +3,7 @@ import requests
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
-payload = {'temp': '30.33', 'pres': '1050.5', 'humi': '50.33'}
+payload = {'temp': '34.33', 'pres': '1050.5', 'humi': '50.33'}
 
 # send the data in a POST request with 10 second timeout
 # r = requests.post('http://localhost/php/data_receiver.php', data=payload, timeout=10)
