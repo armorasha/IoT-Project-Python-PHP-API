92d18b35813943b353efde8f786e1f112837a641
armoz14 <38024713+armorasha@users.noreply.github.com>
2020-01-21 00:13:37 +1030

    db cleaner added

--- php/db_cleaner.php
+++ php/db_cleaner.php
@@ -1,9 +1,6 @@
 <?php
     
 //receiving data in a POST request from IoT device using python's requests library
-$temperature = $_POST['temp'];
-$pressure = $_POST['pres'];
-$humidity = $_POST['humi'];
 $client_key = $_POST['key'];
 
 //get the server key and check with POSTed client key
@@ -17,13 +14,12 @@ if ($server_key === $client_key) //when authentication passes write to db
         require_once('../php/conn_php_math_db.php');
 
         //writing the received data in the database
-        $query = "INSERT INTO sensehat_readings (timestamp, temperature, pressure, humidity)" .
-            " VALUES (CONVERT_TZ(NOW(),'SYSTEM','Australia/Adelaide'), " . $temperature . ", " . $pressure . ", " . $humidity . ")";
+        $query = "DELETE FROM sensehat_readings WHERE reading_id NOT IN(SELECT * FROM(SELECT reading_id FROM sensehat_readings ORDER BY timestamp DESC LIMIT 10) r)";
         $result = mysqli_query($conn, $query) or die("R_Invalid Error" . mysqli_error($conn));
 
         mysqli_close($conn);
 
-        echo "POST data written to database after Auth";
+        echo "Database cleaned after Auth";
     }
     else {
         echo "POST Authentication failed";
